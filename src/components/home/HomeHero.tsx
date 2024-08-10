d1799ea73adbe92693d60c7242f7db876b82bfbe
zakarpatrik <zakipepi@gmail.com>
2024-08-10 15:20:12 +0200

    fix(homepage): adjust home hero component for all device sizes

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -6,10 +6,10 @@ export interface HomeDataProps {
 
 const HomeHero = (props: HomeDataProps) => {
     return (
-        <div className="w-full bg-fixed bg-cover" style={{backgroundImage: 'url(src/assets/bazilika3.webp)'}}>
+        <div className="w-full bg-fixed bg-cover bg-center" style={{backgroundImage: 'url(src/assets/bazilika3.webp)'}}>
             <div className='bg-gradient-to-b from-slate-900/60 to-slate-500/40'>
-                <div className="container flex relative w-full items-center justify-center h-screen">
-                    <h1 className="text-[4.5rem] font-semibold text-white text-center font-title w-2/3 z-10 pb-40 px-0">
+                <div className="container flex relative w-full items-center justify-center aspect-[10/16] md:aspect-square lg:aspect-[17/10]">
+                    <h1 className="text-[2rem] font-semibold text-white text-center font-title w-full z-10 px-4 md:text-[3.2rem] md:w-2/3 md:px-0 lg:text-[3.8rem] xl:text-[4.5rem]">
                         {props.homeData?.data.attributes.Cim}
                     </h1>
                 </div>
