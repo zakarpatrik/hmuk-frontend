aafc149b66126767fa1c23c353f28a8b02d9ffe7
zakarpatrik <zakipepi@gmail.com>
2024-09-30 23:09:16 +0200

    feat(scroll): add scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header/Header.tsx";
 import Footer from "@/components/footer/Footer.tsx";
-import {Outlet} from "@tanstack/react-router";
+import {Outlet, ScrollRestoration} from "@tanstack/react-router";
 
 const App = () => {
   return (
       <>
         <Header />
+        <ScrollRestoration />
         <Outlet />
         <Footer />
       </>
